d1a78c57c65a54fe2e5edabd043a4a5eb7b41f3b
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-03-04 14:48:11 -0500

    initial commit

---
+++ src/main/java/net/dahanne/boot3cloudfoundryclientdemo/Boot3CloudfoundryClientDemoApplication.java
@@ -0,0 +1,13 @@
+package net.dahanne.boot3cloudfoundryclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot3CloudfoundryClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot3CloudfoundryClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dahanne/boot3cloudfoundryclientdemo/Boot3CloudfoundryClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dahanne.boot3cloudfoundryclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot3CloudfoundryClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
